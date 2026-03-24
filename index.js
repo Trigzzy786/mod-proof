7a0e644c1e0c405ad023ee4b44f5d3391d6f9237
Trigzzy786 <triglol786@gmail.com>
2026-03-24 17:35:41 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+{
+  "_0x6f2a": {
+    "tag": "ZGlyZWN0IG1lc3NhZ2Ugc3Bvb2Zlcg==",
+    "alias": "direct message spoofer",
+    "cfg": {
+      "0x1": "!!dGhpcy1sb29rcy1yYW5kb20hIQ==!!",
+      "0x2": [404, "0xFFEE", false, null, "??:://"],
+      "0x3": {
+        "inner_a": "\u0064\u0069\u0072\u0065\u0063\u0074",
+        "inner_b": "\x6d\x65\x73\x73\x61\x67\x65",
+        "inner_c": "c3Bvb2Zlcg=="
+      }
+    }
+  },
+  "x__$": {
+    "scramble": [
+      {"k": "a1", "v": "###@@@!!!"},
+      {"k": "b2", "v": "==--==--"},
+      {"k": "c3", "v": "e30="}
+    ],
+    "bin": "0110010001101101",
+    "hex": "0x646d7370"
+  },
+  "__obf_meta": {
+    "seed": 928374,
+    "hash": "5d41402abc4b2a76b9719d911017c592",
+    "comment": "direct message spoofer::shadow-layer"
+  }
+}
